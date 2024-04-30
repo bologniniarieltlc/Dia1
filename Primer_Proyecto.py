a327adfe0449de95fb265d28c638f8ae67520ebb
Arieu Trabajo <bolognini.ariel@outlook.com>
2024-04-30 11:20:13 -0300

    Nuevo commit

--- Primer_Proyecto.py
+++ Primer_Proyecto.py
@@ -1,6 +1,13 @@
+
+
 print("Hola Mundo")
 print ("100 + 50")
 print (100 + 50)
 print ("Hola" + " " + "Ariel")
 print ("Me llamo  \"Ariel\"  ") # \se puede poner caracteres especiales
 print("    Esta es la primera linea     \n    y esta es la segunda linea")
+
+print("e agora?")
+
+print ("lalala")
+
